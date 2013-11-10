d6053007d554ca68688865af793e7078d10c1f47
Sandeep Kumar Vemula <bittu4u.4ever@gmail.com>
2013-11-10 14:29:40 +0530

    First Teaser

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-setTimeout(function(){
-	$("#loader").addClass("pt-page-scaleDown");
-	$("#main").addClass("pt-page-moveFromBottom").show();
-	$("#loader").hide();
-	}, 2000);
-};
